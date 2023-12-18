f9ceba9ad0e9e8f15e77246a95f4bb86b40c3aa0
João Gabriel <joaogabrielsavioli6@gmail.com>
2023-12-18 15:50:34 -0300

    Adding class 35 and 36

---
+++ dia_2/aula_35.js
@@ -0,0 +1,12 @@
+// Em um for loop, é necessário fornecer três informações:
+// 1ª - Variável
+// 2ª - Condição
+// 3ª - Incremento
+
+for (i = 1; i <= 10; i++){
+    console.log('Número ' + i)
+}
+
+// Aqui eu defini a variável, que é a letra i (chamamos de index)
+// Depois eu coloquei a condicação, enquanto a variável i for menor ou igual a 10
+// Depois o incremento, ou também ação, '++', que vai adicionando mais um 
---
+++ dia_2/aula_36.js
@@ -0,0 +1,15 @@
+// Como fazer um while loop?
+// Ele faz a mesma coisa que o for loop, só que com algumas coisas diferentes
+
+let i = 1
+
+// Você define a variável antes de começar a operação
+
+while (i <= 10) {
+    console.log('Número', i)
+    i++
+}
+
+// (i >= 10) = define que o index tem que ir até o número 10
+// console.log('Número', i) = define o que deve retornar no terminal
+// i++ = Ir contando de um mais um 
