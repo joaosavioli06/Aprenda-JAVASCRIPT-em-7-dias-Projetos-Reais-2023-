89f63cfa287818293ad69182e7190cc11ebfd935
João Gabriel <joaogabrielsavioli6@gmail.com>
2024-01-22 21:34:01 -0300

    Adding the 2nd project's folder

---
+++ projeto_2/index.js
@@ -0,0 +1,21 @@
+// Não precisa colocar nomes nas funções
+
+// Criando uma função que recolhe os dados de ID's no HTML
+// A variável "conta" pega o valor gasto 
+// A variável "gorjeta" é um input range de 0 a 100 %
+// A variável valor é o valor da gorjeta a ser dada ao garçom
+
+function suaConta () {
+    let conta = Number(document.getElementById('yourBill').value)
+    let gorjeta = document.getElementById('tipInput').value
+    let valor = document.getElementById('splitInput').value
+     
+
+}
+
+
+// Fazer 3 cálculos:
+// Um cálculo do valor da gorjeta
+// Cálculo da conta do restaurante + gorjeta
+// Calculo de quanto cada um deve pagar pelo valor total da conta do restaurante
+
