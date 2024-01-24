597d244b8690bad02d4fb4e5567f960fb6fcc6ca
João Gabriel <joaogabrielsavioli6@gmail.com>
2024-01-23 21:28:57 -0300

    Logical part 99% completed

--- projeto_2/index.js
+++ projeto_2/index.js
@@ -5,20 +5,39 @@
 // A variável "gorjeta" é um input range de 0 a 100 %
 // A variável valor é o valor da gorjeta a ser dada ao garçom
 
+function howMany() {
+    let numPessoas = document.getElementById('splitInput').value
+    
+    if (numPessoas == 1)
+    document.getElementById('splitValue').innerText = numPessoas + ' Pessoa'
+    else 
+    document.getElementById('splitValue').innerText = numPessoas + ' Pessoas'
+}
+
+
+function casas(num){
+    return num.toFixed(2)
+}
+
 function suaConta () {
     let conta = Number(document.getElementById('yourBill').value)
     let gorjeta = document.getElementById('tipInput').value
-    let valor = document.getElementById('splitInput').value
+    let dividirGorjeta = document.getElementById('splitInput').value
      
     let valorGorjeta = conta * (gorjeta / 100)
     let contaTotal = conta + valorGorjeta
+    let gorjetaTotal = contaTotal / dividirGorjeta
 
+
+    document.getElementById('tipPercent').innerText = gorjeta + ' %'
+    document.getElementById('tipValue').innerText = 'R$ ' + casas(valorGorjeta)
+    document.getElementById('totalWithTip').innerText = 'R$ ' + contaTotal
+    document.getElementById('splitInput').innerText = howMany(splitValue)
+    document.getElementById('billEach').innerText = 'R$ ' + casas(gorjetaTotal)
     
 }
-
 
 // Fazer 3 cálculos:
 // Um cálculo do valor da gorjeta
 // Cálculo da conta do restaurante + gorjeta
 // Calculo de quanto cada um deve pagar pelo valor total da conta do restaurante
-
